87604e08b24bcc19723b3bd6183ae9832d066e07
joaoviictorti <joaovictorti08@gmail.com>
2025-08-22 23:47:20 -0300

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -11,25 +11,17 @@
 
 extern crate alloc;
 
-mod com;
-mod host_control;
-
-/// Defines data structures and descriptions for manipulating and interacting with the CLR.
 pub mod data;
-
-/// Manages specific error types used when interacting with the CLR and COM APIs.
 pub mod error;
 
-/// Implementing the core CLR loading and interaction logic.
-mod clr;
-pub use clr::*;
-
-/// Responsible for executing powershell code using CLR
+mod com;
+mod host_control;
 mod pwsh;
-pub use pwsh::*;
-
-/// Utilities
 mod utils;
+mod clr;
+
+pub use clr::*;
+pub use pwsh::*;
 pub use utils::*;
 
 /// Type alias for `Result` with `ClrError` as the error type.
