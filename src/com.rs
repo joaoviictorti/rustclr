fb88012993c87aea19face54f25b514031cc3cf6
joaoviictorti <joaovictorti08@gmail.com>
2025-09-11 13:31:41 -0300

    docs: remove documentation

--- src/com.rs
+++ src/com.rs
@@ -18,7 +18,7 @@ pub const CLSID_COR_RUNTIME_HOST: GUID = GUID::from_u128(0xCB2F6723_AB3A_11d2_9C
 /// ICLR Runtime Host (runtime hosting interface)
 pub const CLSID_ICLR_RUNTIME_HOST: GUID = GUID::from_u128(0x90F1A06E_7712_4762_86B5_7A5E_BA6B_DB02);
 
-/// Function type for creating instances of the CLR (Common Language Runtime).
+/// Function type for creating instances of the CLR.
 type CLRCreateInstanceType = fn(
     clsid: *const GUID,
     riid: *const GUID,
@@ -26,31 +26,21 @@ type CLRCreateInstanceType = fn(
 ) -> HRESULT;
 
 /// Function type for retrieving the current thread's CLR identity.
-pub(crate) type CLRIdentityManagerType = fn(riid: *const GUID, ppv: *mut *mut c_void) -> HRESULT;
+pub(crate) type CLRIdentityManagerType = fn(
+    riid: *const GUID, 
+    ppv: *mut *mut c_void
+) -> HRESULT;
 
 /// Helper function to create a CLR instance based on the provided CLSID.
-///
-/// # Arguments
-///
-/// * `clsid` - A pointer to the GUID of the CLR class to instantiate.
-///
-/// # Returns
-///
-/// * `Ok(T)` - if the instance is created successfully, with `T` representing the interface requested.
-/// * `Err(ClrError)` - if the function fails to load `CLRCreateInstance` or if the instance creation fails.
 pub fn CLRCreateInstance<T>(clsid: *const GUID) -> Result<T>
 where
     T: Interface,
 {
     // Load the 'mscoree.dll' library and get the address of the 'CLRCreateInstance' function.
     let CLRCreateInstance = CLR_CREATE_INSTANCE.call_once(|| {
-        // Load 'mscoree.dll' and get the address of 'CLRCreateInstance'
         let module = LoadLibraryA(obfstr::obfstr!("mscoree.dll"));
         if !module.is_null() {
-            // Get the address of 'CLRCreateInstance'
             let addr = GetProcAddress(module, 2672818687u32, Some(dinvk::hash::murmur3));
-
-            // Transmute the address to the function type
             return Some(unsafe {
                 core::mem::transmute::<*mut c_void, CLRCreateInstanceType>(addr)
             });
